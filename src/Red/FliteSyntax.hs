1af2453737bca7bca7c8336f4e0b25cc87ebd590
Benji Jones <benji.s.jones@gmail.com>
2015-01-15 20:47:55 +0000

    add Flite Syntax file

---
+++ src/Red/FliteSyntax.hs
@@ -0,0 +1,40 @@
+module Red.Syntax1 where
+
+type Prog = [Decl]
+
+data Decl = Func { funcName :: Id
+                 , funcArgs :: [Pat]
+                 , funcRhs  :: Exp }
+          | Other String
+  deriving Show
+
+type Id = String
+
+data Exp = App Exp [Exp]
+         | Case Exp [Alt]
+         | Let [Binding] Exp
+         | Var Id
+         | Con Id
+         | Fun Id
+         | Int Int
+         | Wld -- Wildcard '_'
+
+           -- The following may be introduced by various transformations,
+           -- but not by the parser.
+         | Bottom
+         | Alts [Id] Int
+         | Ctr Id Int Int
+         | Lam [Id] Exp
+
+           -- For speculative evaluation of primitive redexes.
+         | PrimApp Id [Exp]
+         | Prim Id
+  deriving (Eq, Show)
+
+type Pat = Exp
+
+type Alt = (Pat, Exp)
+
+type Binding = (Id, Exp)
+
+type App = [Exp]
